337e7b0b249f94a46005692095adc1dfcf42fde6
wangyuzhen <wb_wangyuzhen02@meituan.com>
2022-12-31 21:26:57 +0800

    第一次修改后提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,11 @@
+import java.sql.SQLOutput;
+
 /**
  * @author wb_wangyuzhen02@meituan.com
  * @date 2022年12月31日 17:53
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("练习提交");
+    }
 }
