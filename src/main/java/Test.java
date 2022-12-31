8c9263b27ac51a9bdccc41c5ac80d603f0e71de4
wangyuzhen <wb_wangyuzhen02@meituan.com>
2022-12-31 22:52:50 +0800

    demo1提交了一句话

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("练习提交");
         System.out.println("demo添加一句话");
+        System.out.println("demo1添加一句话");
     }
 }
