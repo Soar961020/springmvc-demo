70004fa21de98cc5d03290cbc3699157f7cea361
wangyuzhen <wb_wangyuzhen02@meituan.com>
2022-12-31 23:46:58 +0800

    demo1第二次提交代码

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -10,5 +10,6 @@ public class Test {
         System.out.println("demo添加一句话");
         System.out.println("demo第二次添加一句话");
         System.out.println("demo提交第二句话");
+        System.out.println("demo1第二次提交代码");
     }
 }
