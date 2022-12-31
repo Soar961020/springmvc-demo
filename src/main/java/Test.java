e9989ce9bf081331db65fb9c8d040257e6702447
wangyuzhen <wb_wangyuzhen02@meituan.com>
2022-12-31 23:53:24 +0800

    demo1第三次提交代码

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,11 +6,6 @@ import java.sql.SQLOutput;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("练习提交");
-        System.out.println("demo添加一句话");
-        System.out.println("demo第二次添加一句话");
-        System.out.println("demo提交第二句话");
-        System.out.println("demo1第二次提交代码");
-        System.out.println("demo第二次提交代码");
+        System.out.println("demo1提交第三次代码");
     }
 }
